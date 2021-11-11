c3cd2a31f518f7345ee705dc19647c0a58ba96d7
lanhhet <dsheibak@mail.ru>
2021-11-11 21:57:50 +0300

    oap lr 4

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,121 @@
+﻿/* 
+    Шейбак Дарья, гр3 курс 2, ИСИТ
+   
+    1) Создать заданный в варианте класс. Определить в классе необходимые методы, конструкторы, индексаторы и заданные перегруженные операции. 
+    Написать программу тестирования, в которой проверяется использование перегруженных операций.
+    2) Добавьте в свой класс вложенный объект Owner, который содержит Id, имя и организацию создателя. Проинициализируйте его.
+    3) Добавьте в свой класс вложенный класс Date (дата создания). Проинициализируйте.
+    4) Создайте статический класс StatisticOperation, содержащий 3 метода для работы с вашим классом (по варианту п.1): сумма, разница между максимальным и минимальным, подсчет количества элементов. 
+    5) Добавьте к классу StatisticOperation методы расширения для типа string и вашего типа из задания№1. См. задание по вариантам.
+    
+    в.22
+    Класс - очередь Queue. Дополнительно перегрузить следующие операции: 
+    / - добавить элемент; 
+    ++ - извлечь элемент; 
+    false - проверка, на содержание четных элементов в очереди; 
+    явный int() - количество положительных элементов в очереди;
+
+Методы расширения:
+1) Выделение первого числа, содержащегося в строке
+2) Обнуление отрицательных элементов очереди
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // протестируем клаcc Queue
+
+            Queue MyQueue = new Queue();
+
+            /* тестируем неперегруженный функционал - стандартные методы
+            MyQueue.pushQueue(194);
+            MyQueue.pushQueue(-8);
+            MyQueue.pushQueue(21);
+            MyQueue.pushQueue(505);
+
+            MyQueue.printQueue(); // выведем очередь на консоль
+
+            if (MyQueue.PresenceEven()) Console.WriteLine("В очереди есть четные элементы");
+            else Console.WriteLine("В очереди нет четных элементов");
+
+            Console.WriteLine("Количество положительных элементов в очереди : {0}", MyQueue.CulcPos());
+
+            Console.WriteLine("Извлечем из очереди элемент очереди : {0}", MyQueue.popQueue());
+            Console.WriteLine("Извлечем из очереди элемент очереди : {0}", MyQueue.popQueue());
+
+            MyQueue.printQueue(); // выведем очередь на консоль
+            */
+
+            MyQueue = MyQueue / 194;
+            MyQueue = MyQueue / -8;
+            MyQueue = MyQueue / 21;
+            MyQueue = MyQueue / 505;
+
+            MyQueue.printQueue(); // выведем очередь на консоль
+
+            if (MyQueue) Console.WriteLine("В очереди есть четные элементы");
+            else Console.WriteLine("В очереди нет четных элементов");
+
+            Console.WriteLine("Количество положительных элементов в очереди : {0}", (int)MyQueue);
+
+            Console.WriteLine("\n-- извлечем из очереди два элемента");
+            MyQueue = MyQueue++;
+            MyQueue = MyQueue++;
+
+            MyQueue.printQueue(); // выведем очередь на консоль
+
+            if (MyQueue) Console.WriteLine("В очереди есть четные элементы");
+            else Console.WriteLine("В очереди нет четных элементов");
+
+            Console.WriteLine("Количество положительных элементов в очереди : {0}", (int)MyQueue);
+
+            Console.WriteLine("\n-- извлечем из очереди оставшиеся два элемента");
+            MyQueue = MyQueue++;
+            MyQueue = MyQueue++;
+
+            MyQueue.printQueue(); // выведем очередь на консоль
+
+            // протестируем дату создания и владельца очереди
+
+            Console.WriteLine("Очередь создана (дата) : {0}", MyQueue.CreationDate.day);
+            Console.WriteLine("Информация о владельце очереди : ID {0} {1} {2}", MyQueue.qowner.ID, MyQueue.qowner.name, MyQueue.qowner.organisation);
+
+            // протестируем статический класс StatisticOperation
+            Console.WriteLine("\n-- протестируем статический класс StatisticOperation - занесем в очередь четыре элемента");
+            MyQueue = MyQueue / 8;
+            MyQueue = MyQueue / -2;
+            MyQueue = MyQueue / 11;
+            MyQueue = MyQueue / 9;
+            MyQueue = MyQueue / 23;
+
+            MyQueue.printQueue(); // выведем очередь на консоль
+            Console.WriteLine("Сумма значений элементов : {0}", StatisticOperation.QSUM(MyQueue));
+            Console.WriteLine("Разница между максимальным и минимальным значением элементов : {0}", StatisticOperation.DIFF_MAX_MIN(MyQueue));
+            Console.WriteLine("Количество элементов : {0}", StatisticOperation.QPOWER(MyQueue));
+            //
+            String someStr = "This building will have 5 or 9 or  levels";
+            int first_number = someStr.firstNumber(MyQueue);
+            if (first_number == -1) Console.WriteLine("Ни один из элементов очереди не содержится в строке '{0}'", someStr);
+            else Console.WriteLine("Первое число (элемент очереди), содержащееся в строке '{0}' : {1}", someStr, first_number);
+            //
+            Console.WriteLine("\n-- при помощи метода расширения обнулим отрицательные элементы очереди");
+            someStr.zeroNegatives(MyQueue);
+            MyQueue.printQueue(); // выведем очередь на консоль
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Queue.cs
@@ -0,0 +1,422 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Очередь - одномерная структура данных, для которой загрузка и извлечение элементов осуществляется
+ с помо-щью указателей начала извлечения (head) и конца (tail) очереди в соответствии с правилом 
+ FIFO ("FIFO" - "первым введен, первым выведен"), т. е. включение производится с одного, а исключение – с другого конца.
+
+*/
+
+public class Queue
+{
+    // вложенный класс
+    public class Date   
+    {
+
+        private DateTime date;
+
+        public Date() // конструктор
+        {
+            date = DateTime.Today; // присваиваем текущую дату
+        }
+
+        // определим для члена date специальные методы доступа - Свойства - они обеспечивают простой доступ к полям классов и структур, позволяют узнать их значение или выполнить их установку
+        public DateTime day      // свойство для даты
+        {
+
+            get { return date; }
+
+            set { date = value; }
+
+        }
+
+    }
+
+    // вложенный объект 
+    public struct Owner
+    {
+        public int ID;         // ID создателя очереди
+        public String name;    // Имя создателя очереди
+        public String organisation;    // Организация создателя очереди
+    };
+
+    // реализация очереди
+
+    public class QNode    // элемент очереди
+    {
+        public int value;   // данные элемента очереди                               
+        public QNode next;  // следующий элемент очереди
+    };
+
+    public QNode head;  // вершина очереди
+    public int QueueSize; // текущая размерность очереди (кол-во элементов очереди)
+    public Date CreationDate; // дата создания очереди
+    public Owner qowner; // информация о владельце очереди
+
+    public Queue() // конструктор
+    {
+        // инициализируем члены-данные Очереди
+        head = null;
+        QueueSize = 0;
+        // отмечаем дата создания очереди
+        CreationDate = new Date();
+        // заносим информацию о владельце очереди
+        qowner = new Owner();
+        qowner.ID = 100;
+        qowner.name = "Шейбак Дарья";
+        qowner.organisation = "БГТУ, ФИТ";
+    }
+
+    public void pushQueue(int ival)
+    {
+        /* постановка элемента в конец очереди */
+
+        if (head == null) // очередь пуста - новый элемент становится первым элементом очереди
+        {
+
+            head = new QNode(); // создаем новый элемент
+
+            head.value = ival;
+
+            head.next = null;
+        }
+
+        else // очередь непуста
+        {
+
+            // переходим на последний элемент очереди:
+            QNode tailnode = head;
+            while (tailnode.next != null) tailnode = tailnode.next;
+
+            // присоединяем новый элемент
+            tailnode.next = new QNode(); // создаем новый элемент
+            tailnode.next.value = ival;
+            tailnode.next.next = null;
+
+        }
+
+        QueueSize++; // инкрементируем счетчик количества элементов очереди
+    }
+
+    public int popQueue()
+    {
+        /* извлечение элемента с начала очереди */
+
+        int ret_val;
+
+        if (head == null) // очередь пуста
+        {
+
+            ret_val = 0; // всегда возвращаем 0 в случае пустой очереди
+
+        }
+
+        else // очередь непуста
+        {
+
+            ret_val = head.value; // возвращаемое значение начального элемента
+
+            // смещаем вершину очереди
+            if (head.next == null) head = null; // в очереди был ровно один элемент - опустошили очередь
+
+            else
+            {
+                head = head.next; // вычитали элемент - смещаем вершину очереди
+
+            }
+
+        }
+
+        QueueSize--; // декрементируем счетчик количества элементов очереди
+
+        return ret_val;
+    }
+
+    public int getQueueSize()
+    {
+        /* возвращает размерность очереди (кол-во элементов очереди) */
+
+        return QueueSize;
+
+    }
+
+    public void printQueue()
+    {
+        /* вывод элементов очереди на консоль */
+
+        Console.WriteLine();
+
+        if (getQueueSize() == 0) Console.Write("Очередь пуста.");
+
+        else // очередь непуста
+        {
+
+            // проходим от начала до конца очереди, выводя ее элементы:
+            QNode tailnode = head;
+
+            do
+            {
+                Console.Write("{0} ", tailnode.value);
+
+                tailnode = tailnode.next;
+
+            } while (tailnode != null);
+
+        } // очередь непуста
+
+        Console.WriteLine();
+
+    }
+
+    public bool PresenceEven()
+    {
+        /* проверка на содержание четных элементов в очереди */
+
+        bool ret_val = false;
+
+        if (getQueueSize() == 0) ret_val = false; // очередь пуста
+
+        else // очередь непуста
+        {
+
+            // проходим от начала до конца очереди, выводя ее элементы:
+            QNode tailnode = head;
+
+            do
+            {
+                if (tailnode.value % 2 == 0)
+                {
+
+                    ret_val = true;
+
+                    break; // from while()
+
+                }
+
+                tailnode = tailnode.next;
+
+            } while (tailnode != null);
+
+        } // очередь непуста
+
+        return ret_val;
+
+    }
+
+    public int CulcPos()
+    {
+        /* подсчет количества положительных элементов в очереди */
+
+        int qpos = 0; // счетчик количества положительных элементов в очереди
+
+        if (getQueueSize() == 0) qpos = 0; // очередь пуста
+
+        else // очередь непуста
+        {
+
+            // проходим от начала до конца очереди, выводя ее элементы:
+            QNode tailnode = head;
+
+            do
+            {
+                if (tailnode.value >= 0) qpos++;
+
+                tailnode = tailnode.next;
+
+            } while (tailnode != null);
+
+        } // очередь непуста
+
+
+        return qpos;
+
+    }
+
+    /* перегрузим требуемые в условии задания операторы
+
+    / - добавить элемент; 
+    ++ - извлечь элемент; 
+    false - проверка, на содержание четных элементов в очереди;
+    приведение типа - явный int () - количество положительных элементов в очереди;
+
+    */
+
+    public static Queue operator /(Queue x, int y)
+    {
+        // добавить элемент 
+
+        x.pushQueue(y);
+
+        return x;
+    }
+
+    public static Queue operator ++(Queue x)
+    {
+        // извлечь элемент
+
+        x.popQueue();
+
+        return x;
+    }
+
+    public static bool operator false(Queue x)
+    {
+        // проверка на содержание четных элементов в очереди
+
+        // возврат логического значения true (в очереди есть четные элементы) или false (в очереди нет четных элементов)
+
+        return x.PresenceEven();
+    }
+
+    public static bool operator true(Queue x)
+    {
+        // проверка на содержание четных элементов в очереди
+
+        // возврат логического значения true (в очереди есть четные элементы) или false (в очереди нет четных элементов)
+
+        return x.PresenceEven();
+    }
+
+
+    public static implicit operator int(Queue x)
+    {
+        // подсчет количества положительных элементов в очереди
+
+        return x.CulcPos();
+    }
+
+}
+
+public static class StatisticOperation
+{
+
+    /* создадим статический класс StatisticOperation, содержащий 3 метода для работы с классом Queue :
+       - сумма значений элементов;
+       - разница между максимальным и минимальным значением элементов;
+       - подсчет количества элементов.
+    */
+
+    /* В сравнении с нестатическим классом, статический класс имеет следующие свойства(отличия) :
+       - нельзя создавать объекты статического класса;
+       - статический класс должен содержать только статические члены.
+    */
+
+
+    public static int QSUM(Queue q)
+    {
+        /* возвращает сумма значений элементов очереди */
+
+        int ret_val = 0;
+
+        if (q.getQueueSize() == 0) return ret_val; // очередь пуста
+
+        // проходим от начала до конца очереди :
+        Queue.QNode tailnode = q.head;
+
+        do
+        {
+            ret_val = ret_val + tailnode.value;
+
+            tailnode = tailnode.next;
+
+        } while (tailnode != null);
+
+        return ret_val;
+
+    }
+
+    public static int DIFF_MAX_MIN(Queue q)
+    {
+        /* возвращает разницe между максимальным и минимальным значением элементов */
+
+        int ret_val = 0;
+
+        if (q.getQueueSize() == 0) return ret_val; // очередь пуста
+
+        int max_val = q.head.value, min_val = q.head.value; // инициализируем значением первого элемента очереди
+
+        // проходим от начала до конца очереди :
+        Queue.QNode tailnode = q.head;
+
+        do
+        {
+            if (tailnode.value > max_val) max_val = tailnode.value;
+            if (tailnode.value < min_val) min_val = tailnode.value;
+
+            tailnode = tailnode.next;
+
+        } while (tailnode != null);
+
+        ret_val = max_val - min_val;
+
+        return ret_val;
+
+    }
+
+    public static int QPOWER(Queue q)
+    {
+        /* возвращает количество элементов очереди */
+
+        return q.getQueueSize();
+
+    }
+
+    /* Методы расширения:
+       - выделение первого числа, содержащегося в строке
+       - обнуление отрицательных элементов очереди
+    */
+
+    public static int firstNumber ( this String st, Queue q)
+    {
+        /* метод расширения: выделение первого числа, содержащегося в строке, -1 - таких чисел нет */
+
+        int ret_val = -1;
+
+        if (q.getQueueSize() == 0) return ret_val; // очередь пуста
+
+        // проходим от начала до конца очереди, проверяя ее элементы на присутствие в строке:
+        Queue.QNode tailnode = q.head;
+
+        do
+        {
+
+            for (int i = 0; i < st.Length; i++)
+            {
+                if (tailnode.value == (int)Char.GetNumericValue(st[i]))
+                {
+                    return tailnode.value;
+                }
+            }
+
+        tailnode = tailnode.next;
+
+        } while (tailnode != null);
+
+        return ret_val;
+
+    }
+
+    public static void zeroNegatives(this String st, Queue q)
+    {
+        /* метод расширения: обнуление отрицательных элементов очереди */
+
+        if (q.getQueueSize() == 0) return ; // очередь пуста
+
+        // проходим от начала до конца очереди :
+        Queue.QNode tailnode = q.head;
+
+        do
+        {
+            if (tailnode.value < 0) tailnode.value = 0;
+            
+            tailnode = tailnode.next;
+
+        } while (tailnode != null);
+
+    }
+
+}
